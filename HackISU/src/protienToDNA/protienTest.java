71c712bec2194608eb9339963bf2a1e4893fa43e
abradih <abradih@gmail.com>
2015-04-11 10:14:49 -0500

    Day 2 currrent

--- HackISU/src/protienToDNA/protienTest.java
+++ HackISU/src/protienToDNA/protienTest.java
@@ -3,7 +3,7 @@ package protienToDNA;
 public class protienTest {
 
 	public static void main(String[] args) {
-		System.out.println(ReadAminoAcids.DNAsequence("zr"));
+		System.out.println(ReadAminoAcids.DNAsequence("1 pygyaglqvs pvnenavign rpwweryqpi syklntrsgn eeqfanmvrr cnnvgvrtyv 61 dvvfnhmaan ggttgtggsn adpssksfpa vpfssldfnp tcaitnyadp tnvrncelvg121 lrdlnqgnsw vrdkivdfln hltdlgvagf rvdaakhmwp gdleaiygrl nnlntnhgfs181 sgakpyifqe vidlgeais kseytglgai tefrhsdsig kvfrgkdqlr yltnwgtswg241 faasdrslvf vdnhdnqrgh gaggadvlty kvp"));
 
 	}
 
