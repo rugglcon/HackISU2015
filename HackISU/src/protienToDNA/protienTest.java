68a229012d0cd2dda6787b4c04184cb892a542b9
abradih <abradih@gmail.com>
2015-04-11 10:18:14 -0500

    Test with trouble with the copy and paste from BLAST

--- HackISU/src/protienToDNA/protienTest.java
+++ HackISU/src/protienToDNA/protienTest.java
@@ -4,7 +4,11 @@ public class protienTest {
 
 	public static void main(String[] args) {
 		System.out.println(ReadAminoAcids.DNAsequence("1 pygyaglqvs pvnenavign rpwweryqpi syklntrsgn eeqfanmvrr cnnvgvrtyv 61 dvvfnhmaan ggttgtggsn adpssksfpa vpfssldfnp tcaitnyadp tnvrncelvg121 lrdlnqgnsw vrdkivdfln hltdlgvagf rvdaakhmwp gdleaiygrl nnlntnhgfs181 sgakpyifqe vidlgeais kseytglgai tefrhsdsig kvfrgkdqlr yltnwgtswg241 faasdrslvf vdnhdnqrgh gaggadvlty kvp"));
-
+		System.out.println(ReadAminoAcids.DNAsequence("1 pygyaglqvs pvnenavign rpwweryqpi syklntrsgn eeqfanmvrr cnnvgvrtyv
+       61 dvvfnhmaan ggttgtggsn adpssksfpa vpfssldfnp tcaitnyadp tnvrncelvg
+      121 lrdlnqgnsw vrdkivdfln hltdlgvagf rvdaakhmwp gdleaiygrl nnlntnhgfs
+      181 sgakpyifqe vidlggeais kseytglgai tefrhsdsig kvfrgkdqlr yltnwgtswg
+      241 faasdrslvf vdnhdnqrgh gaggadvlty kvp");
 	}
 
 }
